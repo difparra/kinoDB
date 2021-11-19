efcb1f33536144b509095b55d899e2f773cdddd4
diegoparra <dfparra11@gmail.com>
2021-11-19 16:16:36 -0500

    initial commit - dependencies setUp - timber setUp - hilt setUp

---
+++ app/src/main/java/com/diegoparra/kinodb/KinoDbApplication.kt
@@ -0,0 +1,28 @@
+package com.diegoparra.kinodb
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+@HiltAndroidApp
+class KinoDbApplication: Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    override fun onCreate() {
+        super.onCreate()
+        asyncInitTimber()
+    }
+
+    private fun asyncInitTimber() {
+        applicationScope.launch {
+            if(BuildConfig.DEBUG) {
+                Timber.plant(Timber.DebugTree())
+            }
+        }
+    }
+
+}
